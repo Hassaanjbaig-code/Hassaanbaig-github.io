f527c853b14e2f359a7b166bbf89a56f052a36a6
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2022-11-19 13:06:01 +0500

    Add local storage

--- app.js
+++ app.js
@@ -146,3 +146,28 @@ connect.forEach((conn) => {
     fetchOneProject(e.target.id);
   });
 });
+
+// For validation
+
+const error = document.querySelector('.error');
+const fname = document.getElementById('name');
+const text = document.getElementById('Complaint');
+
+function store(n, e, t) {
+  const stored = { name: n, email: e, comment: t };
+  const convert = JSON.stringify(stored);
+  localStorage.setItem('data', convert);
+}
+
+function validation() {
+  const email = document.getElementById('email');
+  if (email.value !== email.value.toLowerCase()) {
+    error.textContent = 'Email should be in lower case';
+    return false;
+  }
+  error.textContent = '';
+  return store(fname.value, email.value, text.value);
+}
+
+const submitButton = document.getElementById('Submit');
+submitButton.addEventListener('click', validation);
--- app2.js
+++ app2.js
@@ -1,14 +1,22 @@
 const error = document.querySelector('.error');
+const fname = document.getElementById('name');
+const text = document.getElementById('Complaint');
 
-function validation() {
-  const email = document.getElementById('email');
-  if (email.value !== email.value.toLowerCase()) {
-    error.textContent = 'Email should be in lower case';
-    return false;
-  }
-  error.textContent = '';
-  return true;
+function store(n, e, t) {
+  const stored = { name: n, email: e, comment: t };
+  const convert = JSON.stringify(stored);
+  localStorage.setItem('data', convert);
 }
 
-const submitButton = document.getElementById('Submit');
-submitButton.addEventListener('click', validation);
+// function validation() {
+//   const email = document.getElementById('email');
+//   if (email.value !== email.value.toLowerCase()) {
+//     error.textContent = 'Email should be in lower case';
+//     return false;
+//   }
+//   error.textContent = '';
+//   return store(fname.value, email.value, text.value);
+// }
+
+// const submitButton = document.getElementById('Submit');
+// submitButton.addEventListener('click', validation);
