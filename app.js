341bc6111fce81cfa8f9064e2ece890ea851c4e8
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-17 11:44:19 +0500

    Add Project

--- app.js
+++ app.js
@@ -7,15 +7,19 @@ const projectdata = [
     description: "In this project that you can add your daily schedule. This system also help you mark and unmark what you have finshed. Also you can edit the input that you add in it. You can clear all the input that you add and also clear all the complete task. All of this will be save in local storage that you can see even you refersh the page or come back after sometime",
     description2: "In this project that you can add your daily schedule. This system also help you mark and unmark what you have finshed. Also you can edit the input that you add in it. You can clear all the input that you add and also clear all the complete task. All of this will be save in local storage that you can see even you refersh the page or come back after sometime",
     language: ['HTML', 'CSS', 'JavaScript'],
+    Source_code: 'https://github.com/Hassaanjbaig-code/To-do-webpack',
+    Live_demo: 'https://hassaanjbaig-code.github.io/To-do-webpack/dist/',
   },
   {
     id: 'portfolio1',
-    title: 'ToDo List',
-    frames: ['Todo', 'Font end development', 2022],
-    Image: './Pictures/Todo list.PNG',
-    description: "In this project that you can add your daily schedule. This system also help you mark and unmark what you have finshed. Also you can edit the input that you add in it. You can clear all the input that you add and also clear all the complete task. All of this will be save in local storage that you can see even you refersh the page or come back after sometime ",
-    description2: "In this project that you can add your daily schedule. This system also help you mark and unmark what you have finshed. Also you can edit the input that you add in it. You can clear all the input that you add and also clear all the complete task. All of this will be save in local storage that you can see even you refersh the page or come back after sometime ",
-    language: ['HTML', 'CSS', 'JavaScript'],
+    title: 'Live Stock',
+    frames: ['React', 'Font end development', 2022],
+    Image: './Pictures/Snapshoot Portfolio-d-1.png',
+    description: "About Stock Management project is a sole react-redux project that use Financial Modeling Prep API to display a list of stock and Income statement. Stock show the proce and which industry it belong. Users can access the personal list the profile section.",
+    description2: "In this project that you can view the live stock of the company. You can also view the income statement of the company through you can make sure to Invest in the company or not. You can serach a company by there name. About Stock Management project is a sole react-redux project that use Financial Modeling Prep API to display a list of stock and Income statement. Stock show the proce and which industry it belong. Users can access the personal list the profile section.",
+    language: [ 'React', 'React - Redux' ],
+    Source_code: 'https://github.com/Hassaanjbaig-code/React-Capstone',
+    Live_demo: 'https://deploy-preview-5--glowing-boba-18fd9b.netlify.app/',
   },
   {
     id: 'portfolio2',
@@ -75,11 +79,19 @@ function fetchOneProject(id) {
     const btnmain = document.createElement('div');
     btnmain.classList.add('mobile-button');
     const poplive = document.createElement('button');
-    poplive.innerText = 'See Live';
+    const Live_demo = document.createElement('a');
+    Live_demo.href = project.Live_demo;
+    Live_demo.target = '_blank';
+    Live_demo.innerText = 'See Live';
     poplive.classList.add('mobile-btn');
+    poplive.append(Live_demo);
     const popsource = document.createElement('button');
-    popsource.innerText = 'See Source';
     popsource.classList.add('mobile-btn');
+    const Source_code = document.createElement('a');
+    Source_code.href = project.Source_code;
+    Source_code.target = '_blank';
+    Source_code.innerText = 'See Source';
+    popsource.append(Source_code);
     btnmain.append(
       poplive,
       popsource,
